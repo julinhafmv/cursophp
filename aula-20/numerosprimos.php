cbfafde8b48aa5b93e058ba8c61b4f36be47e0fa
julinhafmv <juleakajuu@gmail.com>
2023-10-26 15:52:13 -0300

    aula20

---
+++ aula-20/numerosprimos.php
@@ -0,0 +1,20 @@
+<?php
+function primos($p){
+    $sequencia = array();
+    $sequencia[0] = 1;
+    $sequencia[1] = 1;
+
+
+for($i = 2; $i < $p; $i++){
+    $sequencia[$i] = $sequencia[$i-1] + $sequencia[$i-2];
+
+}
+
+return $sequencia[$p-1];
+}
+
+echo primos(10);
+
+
+
+?>
