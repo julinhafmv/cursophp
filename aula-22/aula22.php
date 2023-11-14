e7ce4e33728e9d2a5b9874987eadc03cc1f23814
julinhafmv <juleakajuu@gmail.com>
2023-11-14 15:49:37 -0300

    Update aula22.php

--- aula-22/aula22.php
+++ aula-22/aula22.php
@@ -40,8 +40,24 @@ function fibonacci($p){
             <?php
             if (isset($_POST["posicao"])) {
             ?>
-                <h1>resultado</h1>
-                <div class="row mt-2">
+                <h1>Resultado:</h1>
+                <form method="POST">
+                
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Posição</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" name="posicao" placeholder="Posição sequencia de Fibonacci" value="<?php if (isset($_POST["posicao"])) ?>">
+
+                </div>
+
+                <button type="submit" class="btn btn-success">Enviar</button>
+                <button type="submit" class="btn btn-danger">Resetar</button>
+
+            </form>
+            </br>
+            </br>
+        
+
+                <div class="row mt-3">
                     <div class="col-6 offset-3">
                         <div class="alert alert-danger" role="alert"></div>
                         <h3><?php echo $_POST["posicao"];?></h3>
@@ -57,18 +73,7 @@ function fibonacci($p){
             }
             ?>
 
-            <form method="POST">
-                
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Posição</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" name="posicao" placeholder="Posição sequencia de Fibonacci" value="<?php if (isset($_POST["posicao"])) ?>">
-
-                </div>
-
-                <button type="submit" class="btn btn-success">Enviar</button>
-                <button type="submit" class="btn btn-danger">Resetar</button>
-
-            </form>
+            
 
         </div>
 
